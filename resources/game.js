3fbd67aaa5c78b2284abdc5fc899a6eee13702f5
Zach <zachctown@gmail.com>
2013-11-20 23:19:31 -0500

    moving block/player

--- resources/game.js
+++ resources/game.js
@@ -1,4 +1,6 @@
-var canvas = CE.defines("game").ready(function() { 
+var canvas = CE.defines("game").
+    extend(Input).
+    ready(function() { 
     canvas.Scene.call("MyScene");
 });
 
@@ -6,22 +8,34 @@ canvas.Scene.new({
     name: "MyScene",
     materials: {
         images: {
-            grasstile: "images/grasstile.png"
+            grasstile: "resources/images/grasstile.png"
         }
     },
     preload: function(stage, percent) {
         
     },
     ready: function(stage){
-        this.element = this.createElement();
-        this.element.drawImage("grasstile");
-        stage.append(this.element);
-        this.element.x = 20;
-        this.element.y = 20;
+        var player = this.createElement();
+        player.drawImage("grasstile");
+        stage.append(player);
+        player.x = 70;
+        player.y = 200;
+        
+        canvas.Input.keyDown(Input.Left, function() {
+            player.x -= 10;
+        });
+        canvas.Input.keyDown(Input.Right, function() {
+            player.x += 10;
+        }); 
+    
+        
+        
     },
     render: function(stage){
-        this.element.x += 1;
+        
+        
         stage.refresh();
+        
     },
     exit: function(stage){
         
